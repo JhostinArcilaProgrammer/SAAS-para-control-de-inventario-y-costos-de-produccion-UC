df3af73076b1754599b952c43c80ab72fb4a8f00
JhostinArcila <136260130+JhostinArcilaProgrammer@users.noreply.github.com>
2026-06-19 11:44:54 +0000

    Save all workspace changes

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
-if __name__ == "__main__":
-    app.run(debug=True, port=4000)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    return render_template("index.html", title="Helo")
+
+@app.route("/hola")
+def primer_mensaje():
+    return render_template("primer_hola.html", title="Hola")
+    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
+
+
